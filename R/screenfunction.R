1b65b4ece95a4b5102b49e9fea72c7109f7850b1
JeromeAmbroise <jerome.ambroise@uclouvain.be>
2019-06-13 09:47:17 +0200

    Add another function to screen a sequence using blast

--- R/screenfunction.R
+++ R/screenfunction.R
@@ -1,4 +1,4 @@
-#' Screen a sequence
+#' Screen a sequence using Lastz
 #'
 #' You provide a reference and a querry and the function compute the percentage of the reference which is covered by the querry
 #'
@@ -32,5 +32,50 @@ screenLastz <- function(reference,querry)
   else (percentage <- 0)
   unlink('temp',recursive = T)
   return(percentage)
-
 }
+
+
+
+#' Screen a sequence using Blast
+#'
+#' You provide a reference and a querry and the function compute the percentage of the reference which is covered by the querry
+#'
+#' @param reference the reference sequence that you want to screen
+#' @param querry the querry sequence
+#'
+#' @return numeric value of the percentage of the reference sequence which is covered by the querry
+#'
+#' @export
+screenBlast <- function (reference, querry,min.pc.ident)
+{
+  try(unlink("temp", recursive = TRUE))
+  dir.create("temp")
+  dir.create("temp/dbblast")
+  myarg <- paste0("-in ", reference, " -out temp/dbblast/db -dbtype nucl")
+  system2(command = "makeblastdb", args = myarg, stdout = F)
+  myarg <- paste0("-query ", querry, " -db temp/dbblast/db -out temp/blast.txt -num_threads 8 -num_alignments 10 -outfmt \"7 qacc bitscore qlen length pident qstart qend sacc sstart send \"")
+  system2(command = "blastn", args = myarg)
+  blast <- try(read.table("temp/blast.txt", comment.char = "#"), silent = T)
+  if (class(blast) == "data.frame")
+  {
+    colnames(blast) <- c("querry.access", "bitscore", "querry.length", "alignment.lenght", "pc.ident.",
+                         "querry.start", "querry.end", "subject.access", "subject.start", "subject.end")
+    blast <- blast[blast$pc.ident.>min.pc.ident,]
+    start <- blast$subject.start
+    end <- blast$subject.end
+    new.start <- start
+    new.end <- end
+    new.start[start>end] <- end[start>end]
+    new.end[start>end] <- start[start>end]
+    data.frame(start,end,new.start,new.end)
+    GR <- GRanges(seqnames = 'seq',ranges = IRanges(start =new.start ,end = new.end))
+    GR.disjoin <- disjoin(GR)
+    hitlength <- sum(width(GR.disjoin))
+    seqlength <- width(readDNAStringSet(reference))
+    percentage <- 100*round(hitlength/seqlength,3)
+  }
+  else{percentage <- 0}
+  unlink('temp',recursive = T)
+  return(percentage)
+}
+
